3e49c594efac3a00f7efbd9e2975d86d63983bf9
Elizabetha Kadilli <elisabeta.kadilli@surfdome.com>
2016-03-14 17:23:52 +0000

    Adding rspec for this app

--- spec/features/campaigns_spec.rb
+++ spec/features/campaigns_spec.rb
@@ -1,29 +1,43 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "Campaign" do
   context "Index" do
-    before do
-      # create campaigns and stuff
-    end
+    let!(:campaign) { Campaign.create(name: "test") }
 
     it "Lists all existing Campaigns" do
-      visit campaigns_path
-      #  ...
-      # expect the campaigns to be there with whatever info (name etc)
+      visit campaigns_url
+
+      expect(page).to have_content "test"
     end
   end
 
   context "Show" do
-    let(:campaign) { # create 1 campaign }
-
-    before do
-      # not sure if you need this
+    let!(:campaign1)  { Campaign.create(name: "Testing Camaping") }
+    let!(:candidate1) { Candidate.create(name: "Hugo") }
+    let!(:candidate2) { Candidate.create(name: "Anna") }
+    let!(:vote)       { Vote.create(candidate: candidate1, campaign: campaign1, validity: 'during') }
+    let!(:vote1)      { Vote.create(candidate: candidate1, campaign: campaign1, validity: 'during') }
+    let!(:vote2)      { Vote.create(candidate: candidate2, campaign: campaign1, validity: 'during') }
+    let!(:vote3)      { Vote.create(candidate: candidate2, campaign: campaign1, validity: 'pre') }
+    
+    it "Visit a Campaign" do
+      visit campaign_url(campaign1)
     end
 
-    it "Lists all existing Campaigns" do
-      visit campaign_path(campaign)
-      #  ...
-      # expect the campaign to be there with whatever info (candidates votes etc)
+     it "Show the candidates for this campaign" do
+      visit campaign_url(campaign1.id)
+      expect(page).to have_content "Anna"
+      expect(page).to have_content "Hugo"
+    end
+
+    it "Show the candidates scores valid and invalid for this campaign" do
+      visit campaign_url(campaign1.id)
+
+      expect(candidate1.votes.on(campaign1).valid.count).to eq(2)
+      expect(candidate2.votes.on(campaign1).valid.count).to eq(1)
+      expect(candidate1.votes.on(campaign1).invalid.count).to eq(0)
+      expect(candidate2.votes.on(campaign1).invalid.count).to eq(1)
+
     end
   end
 end
